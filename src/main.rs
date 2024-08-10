e443cf98c0937dd33e042ceea2d08ddba2de6f61
h2cone <tw8ape@gmail.com>
2024-08-10 19:44:46 +0800

    feat: Refactor data sink handling in execute function
    Refactor how data is consumed and written to a sink in the execute function.
    Introduce a Sink trait and a FileSink implementation for handling data writing.
    Ensure data is properly consumed and handled based on the sink type.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
-use std::{env, fs, io::Write, time::Instant};
+use std::{
+    env,
+    fs::{self, File},
+    io::Write,
+    time::Instant,
+};
 
 #[tokio::main]
 async fn main() {
@@ -10,23 +15,25 @@ async fn main() {
 }
 
 async fn execute() -> Result<(), Box<dyn std::error::Error>> {
-    let mut more = env::args().nth(1).unwrap_or("".to_string());
-
-    // parse conf.toml
-    let conf_path = env::args().nth(2).unwrap_or("conf.toml".to_string());
+    let conf_path = env::args().nth(1).unwrap_or("conf.toml".to_string());
     let conf_str = std::fs::read_to_string(conf_path)?;
-    let conf = conf_str.parse::<toml::Value>()?;
+    let conf = &conf_str.parse::<toml::Value>()?["douban-game"];
 
-    let url = conf["douban-game"]["search_url"].as_str().unwrap();
-    let result = conf["douban-game"]["search_result_file"].as_str().unwrap();
+    let mut more = conf["more"].as_str().unwrap_or("").to_string();
+    let sink_type: &str = conf["sink_type"].as_str().unwrap();
+    let mut sink: Box<dyn Sink> = match sink_type {
+        "file" => Box::new(FileSink::new(&conf)),
+        _ => panic!("Unsupported sink type"),
+    };
 
+    let url = conf["url"].as_str().unwrap();
     let mut headers = reqwest::header::HeaderMap::new();
     headers.insert("User-Agent", "*".parse()?);
-
     let client = reqwest::Client::new();
 
     let idx = "more";
-    while more != "Null" {
+    let null = serde_json::Value::Null.to_string();
+    while more != null {
         let builder = client
             .get(url)
             .query(&[("sort", "rating"), ("more", &more)])
@@ -35,23 +42,46 @@ async fn execute() -> Result<(), Box<dyn std::error::Error>> {
         let resp = builder.send().await?;
         let body = resp.json::<serde_json::Value>().await?;
 
-        // append to result file
-        let mut file = fs::OpenOptions::new()
-            .create(true)
-            .append(true)
-            .open(result)?;
-
-        println!("Writing more={}", more);
-
-        if let Err(e) = serde_json::to_writer(&file, &body) {
-            eprintln!("Failed to write more={}, err={}", more, e);
-        }
-        writeln!(file)?;
+        sink.consume(&body);
 
         more = body
             .get(idx)
             .unwrap_or(&serde_json::Value::Null)
             .to_string();
+
+        println!("?more={}", more);
     }
     Ok(())
+}
+
+trait Sink {
+    fn consume(&mut self, data: &serde_json::Value);
+}
+
+struct FileSink {
+    file: File,
+}
+
+impl FileSink {
+    fn new(conf: &toml::Value) -> Self {
+        let path = conf["sink_to"].as_str().unwrap();
+
+        let file = fs::OpenOptions::new()
+            .create(true)
+            .append(true)
+            .open(path)
+            .unwrap();
+
+        FileSink { file }
+    }
+}
+
+impl Sink for FileSink {
+    fn consume(&mut self, body: &serde_json::Value) {
+        if let Err(e) = serde_json::to_writer(&self.file, &body) {
+            eprintln!("Failed to write to file, err={}", e);
+        } else {
+            writeln!(self.file).unwrap();
+        }
+    }
 }
